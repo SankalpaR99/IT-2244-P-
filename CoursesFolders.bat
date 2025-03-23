a75d582e92a8f989eae1be75377f32c08a8ee5a1
SankalpaR99 <sankalparathnayake99@gmail.com>
2025-03-23 22:29:38 +0530

    Update CoursesFolders.bat

--- CoursesFolders.bat
+++ CoursesFolders.bat
@@ -1,47 +1,44 @@
-mkdir Course_1
-cd Course_1
-mkdir Subject_1
-mkdir Subject_2
-mkdir Subject_3
-cd..
+mkdir Course_1               :: Create directory for Course_1
+cd Course_1                  :: Change to Course_1 directory
+mkdir Subject_1              :: Create Subject_1 folder inside Course_1
+mkdir Subject_2              :: Create Subject_2 folder inside Course_1
+mkdir Subject_3              :: Create Subject_3 folder inside Course_1
+cd..                         :: Go back to the parent directory
 
-mkdir Course_2
-cd Course_2
-mkdir Subject_1
-mkdir Subject_2
-mkdir Subject_3
-mkdir Subject_4
-cd..
+mkdir Course_2               :: Create directory for Course_2
+cd Course_2                  :: Change to Course_2 directory
+mkdir Subject_1              :: Create Subject_1 folder inside Course_2
+mkdir Subject_2              :: Create Subject_2 folder inside Course_2
+mkdir Subject_3              :: Create Subject_3 folder inside Course_2
+mkdir Subject_4              :: Create Subject_4 folder inside Course_2
+cd..                         :: Go back to the parent directory
 
-mkdir Course_3
-cd Course_3
-mkdir Subject_1
-mkdir Subject_2
-mkdir Subject_3
-mkdir Subject_4
-mkdir Subject_5
-cd..
+mkdir Course_3               :: Create directory for Course_3
+cd Course_3                  :: Change to Course_3 directory
+mkdir Subject_1              :: Create Subject_1 folder inside Course_3
+mkdir Subject_2              :: Create Subject_2 folder inside Course_3
+mkdir Subject_3              :: Create Subject_3 folder inside Course_3
+mkdir Subject_4              :: Create Subject_4 folder inside Course_3
+mkdir Subject_5              :: Create Subject_5 folder inside Course_3
+cd..                         :: Go back to the parent directory
 
-mkdir Course_4
-cd Course_4
-mkdir Subject_1
-mkdir Subject_2
+mkdir Course_4               :: Create directory for Course_4
+cd Course_4                  :: Change to Course_4 directory
+mkdir Subject_1              :: Create Subject_1 folder inside Course_4
+mkdir Subject_2              :: Create Subject_2 folder inside Course_4
+cd..                         :: Go back to the parent directory
 
-cd..
+mkdir Course_5               :: Create directory for Course_5
+cd Course_5                  :: Change to Course_5 directory
+mkdir Subject_1              :: Create Subject_1 folder inside Course_5
+mkdir Subject_2              :: Create Subject_2 folder inside Course_5
+mkdir Subject_3              :: Create Subject_3 folder inside Course_5
+mkdir Subject_4              :: Create Subject_4 folder inside Course_5
+cd..                         :: Go back to the parent directory
 
-mkdir Course_5
-cd Course_5
-mkdir Subject_1
-mkdir Subject_2
-mkdir Subject_3
-mkdir Subject_4
-cd..
+mkdir Course_6               :: Create directory for Course_6
+mkdir Course_7               :: Create directory for Course_7
+mkdir Course_8               :: Create directory for Course_8
+mkdir Course_9               :: Create directory for Course_9
 
-mkdir Course_6
-
-mkdir Course_7
-
-mkdir Course_8
-
-mkdir Course_9
 
