cd6dfca87ce38940fd6cbbb6287e4febf48c189c
SankalpaR99 <sankalparathnayake99@gmail.com>
2025-03-23 22:31:44 +0530

    Update Echo.bat

--- Echo.bat
+++ Echo.bat
@@ -1,6 +1,6 @@
-@echo off
-echo Username:%USERNAME%
-echo Windows Version:
-ver
-set Age\p = How old are you
-pause
+@echo off                            :: Disable command echoing
+echo Username:%USERNAME%             :: Display the current username
+echo Windows Version:                 :: Display the text "Windows Version:"
+ver                                  :: Display the Windows version
+set Age\p = How old are you          :: Prompt for the user's age (note: there's a typo in the variable name)
+pause                                :: Wait for user to press a key before closing
