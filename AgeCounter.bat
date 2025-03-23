d8f6a1d1ab1826cfb6f89ce5c01435b4d1984f9b
SankalpaR99 <sankalparathnayake99@gmail.com>
2025-03-23 22:23:20 +0530

    Update AgeCounter.bat

--- AgeCounter.bat
+++ AgeCounter.bat
@@ -1,6 +1,7 @@
-@echo off
-set /p year=Enter your birth year: 
-set /a age=%date:~10,4% - %year% 
-echo You are %age% years old.
-pause
+@echo off                                   :: Disable command echoing
+set /p year=Enter your birth year:          :: Prompt user for birth year input
+set /a age=%date:~10,4% - %year%            :: Calculate age based on current year and input birth year
+echo You are %age% years old.               :: Display the calculated age
+pause                                       :: Wait for user to press a key before closing
+
 
